34512026e1dc69c8b5f7e1deadd4c4b28392008b
Zeming YU <yuzeming@gmail.com>
2020-05-21 18:10:47 +0800

    Update mt7621.mk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -136,6 +136,14 @@ define Device/mediatek_ap-mt7621a-v60
 endef
 TARGET_DEVICES += mediatek_ap-mt7621a-v60
 
+define Device/xiaoyu_xy-c5
+  DTS := xiaoyu_xy-c5
+  IMAGE_SIZE := $(ralink_default_fw_size_32M)
+  DEVICE_TITLE := XiaoYu XY-C5
+  DEVICE_PACKAGES := kmod-ata-core kmod-ata-ahci kmod-usb3
+endef
+TARGET_DEVICES += xiaoyu_xy-c5
+
 define Device/xzwifi_creativebox-v1
   DTS := CreativeBox-v1
   IMAGE_SIZE := $(ralink_default_fw_size_32M)
